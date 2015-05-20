eb73fdaff29289d0dd6d68e38d1a2092e8c60290
calibr <awcalibr@gmail.com>
2015-05-20 18:03:10 +0300

    few topic tests

--- test/events.js
+++ test/events.js
@@ -167,4 +167,38 @@ describe("Events", function() {
       events1.emit("test");
     }
   });
+
+  it("should call listener with topic", function(done) {
+    var events = new tools.events();
+    function _listener() {
+      events.removeListener("base:topic", _listener);
+      done();
+    }
+    events.on("base:topic", _listener);
+    events.emit("base:topic");
+  });
+
+it("should summary call two listeners of two process with same apps 10 times(same event) with topic", function(done) {
+    var events1 = new tools.events("app");
+    var events2 = new tools.events("app");
+    var listener1Called = 0;
+    var listener2Called = 0;
+    function _listener1() {
+      listener1Called++;
+    }
+    function _listener2() {
+      listener2Called++;
+    }
+    events1.on("base:topic", _listener1);
+    events2.on("base:topic", _listener2);
+    setTimeout(function() {
+      events1.removeListener("base:topic", _listener1);
+      events2.removeListener("base:topic", _listener2);
+      (listener2Called + listener1Called).should.equal(10);
+      done();
+    }, 1000);
+    for(var i = 0; i != 10; i++) {
+      events1.emit("base:topic");
+    }
+  });
 });
