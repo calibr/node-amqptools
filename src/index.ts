328473e58c66379102afed11856cb69a16c0a1e7
denis <awcalibr@gmail.com>
2021-07-11 14:55:13 +0300

    export more classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { AMQPManager } from "./AMQPManager";
 
 export { AMQPManager } from "./AMQPManager";
+export { Task } from "./Task";
+export { TaskManager } from "./TaskManager";
+export { AMQPEventEmitter } from "./EventEmitter";
+export { EventListener } from "./EventListener";
+
+
 export var amqpManager = new AMQPManager();
